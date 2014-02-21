689a960f707da01168711fcedd0594a6de169ed9
senluchen2015 <senluchen2015@u.northwestern.edu>
2014-02-20 18:39:59 -0800

    added dialog to the buttons

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
 /* Javascript Here */
+
+$(function(){
+
+	$("#loginForm").dialog({
+		autoOpen: false,
+		height: 350,
+		width: 300,
+		modal: true,
+		buttons: {
+			"Login": function(){
+			},
+			"Cancel": function(){
+				$(this).dialog("close");
+			}
+		}
+	});
+	$("#loginButton")	
+		.click(function(){
+		$("#loginForm").dialog("open");
+		});
+
+	$("#registerForm").dialog({
+		autoOpen: false,
+		height: 350,
+		width: 300,
+		modal: true
+	});
+
+	$("#registerButton")
+		.click(function(){
+			$("#registerForm").dialog("open");
+		});
+
+});
+
+
