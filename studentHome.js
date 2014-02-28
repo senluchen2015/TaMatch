03f8af15215681b2adce90374af5b6f8b440e1bf
senluchen2015 <senluchen2015@u.northwestern.edu>
2014-02-28 13:56:01 -0800

    changd studentHome

--- studentHome.js
+++ studentHome.js
@@ -3,11 +3,23 @@ function displaySelected(){
 	var options =	document.getElementById("departments").options
 	var selectedIndex =document.getElementById("departments").selectedIndex; 
 	console.log(options[selectedIndex].text);
-	if(options[selectedIndex].text =="ACCOUNT - Accounting"){
-		document.getElementById("courseTable").innerHTML = '<div class="coursesHome">Accounting 101</div>';
+	if(options[selectedIndex].text =="EECS - Elect Engineering & Comp Sci"){
+		var nameEECS=["EECS 111", "EECS 211", "EECS 213"];
+		var instructorEECS = ["Ian Horswill", "Larry Henschen", "Peter Dinda"];
+		var timeEECS = ["12:00-12:50 MWF", "2-2:50 MTuWF", "2-3:20 TuTh"];
+		var html=""; 
+		for(i=0;i<nameEECS.length;i++){
+			if(i%2==0){
+				html+='<div class="coursesHomeEven">'+nameEECS[i]+" "+instructorEECS[i]+" "+timeEECS[i]+'</div>';
+			}
+			else{
+				html+='<div class="coursesHomeOdd">'+nameEECS[i]+" "+instructorEECS[i]+" "+timeEECS[i]+'</div>';
+			}
+		}
+		document.getElementById("courseTable").innerHTML = html;
 	}
 }
 
 function openNotification(){
-		
+
 }
