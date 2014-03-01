1785d2a80359a619188e7859ce8746bb7d25ec47
senluchen2015 <senluchen2015@u.northwestern.edu>
2014-02-28 21:36:12 -0800

    added / changed homepage, login buttons

--- studentHome.js
+++ studentHome.js
@@ -8,18 +8,50 @@ function displaySelected(){
 		var instructorEECS = ["Ian Horswill", "Larry Henschen", "Peter Dinda"];
 		var timeEECS = ["12:00-12:50 MWF", "2-2:50 MTuWF", "2-3:20 TuTh"];
 		var html=""; 
+
 		for(i=0;i<nameEECS.length;i++){
 			if(i%2==0){
-				html+='<div class="coursesHomeEven">'+nameEECS[i]+" "+instructorEECS[i]+" "+timeEECS[i]+'</div>';
+				html+='<div class="coursesHomeEven">'
+				+'<a href="'
+				+nameEECS[i]+ '" style="color:#432F21">'
+				+'<div class="nameCourse">'				
+				+nameEECS[i]+"      "
+				+'</div>' 
+				+'<div class="instructorCourse">'
+				+instructorEECS[i]
+				+'</div>'
+				+'<div class="timeCourse">'
+				+timeEECS[i]
+				+'</div>'
+				+'</a>'
+				+'</div>';
 			}
 			else{
-				html+='<div class="coursesHomeOdd">'+nameEECS[i]+" "+instructorEECS[i]+" "+timeEECS[i]+'</div>';
+				html+='<div class="coursesHomeOdd">'
+				+'<a href="'
+				+nameEECS[i]+ '" style="color:#432F21">'
+				+'<div class="nameCourse">'				
+				+nameEECS[i]+"      "
+				+'</div>' 
+				+'<div class="instructorCourse">'
+				+instructorEECS[i]
+				+'</div>'
+				+'<div class="timeCourse">'
+				+timeEECS[i]
+				+'</div>'
+				+'</a>'
+				+'</div>';
 			}
 		}
 		document.getElementById("courseTable").innerHTML = html;
 	}
 }
 
-function openNotification(){
 
-}
+
+$(document).ready(function(){
+$( "#notification" ).click(function(){
+	$('#notficationList').toggle();
+	console.log("wtf");
+});
+});
